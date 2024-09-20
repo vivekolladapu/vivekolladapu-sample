861120cf6693ec47a2f8cc717305503e20cc5fcc
vivekolladapu <68852720+vivekolladapu@users.noreply.github.com>
2024-09-20 22:11:04 +1000

    Update build-image.sh

--- part4 2/scripts/build-image.sh
+++ part4 2/scripts/build-image.sh
@@ -1,52 +1,24 @@
-# #
-# # Builds a Docker image.
-# #
-# # Environment variables:
-# #
-# #   CONTAINER_REGISTRY - The hostname of your container registry.
-# #   VERSION - The version number to tag the images with.
-# #   NAME - The name of the image to build.
-# #   DIRECTORY - The directory form which to build the image.
-# #
-# # Usage:
-# #
-# #       ./scripts/build-image.sh
-# #
-
-# set -u # or set -o nounset
-# : "$CONTAINER_REGISTRY"
-# : "$VERSION"
-# : "$NAME"
-# : "$DIRECTORY"
-
-# docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ./$DIRECTORY/Dockerfile ./$DIRECTORY
-
 #!/bin/bash
 
-# Builds a Docker image.
-# Environment variables:
-#   CONTAINER_REGISTRY - The hostname of your container registry.
-#   VERSION - The version number to tag the images with.
-#   NAME - The name of the image to build.
-#   DIRECTORY - The directory from which to build the image.
-#
-# Usage:
-#   ./scripts/build-image.sh
+# Exit if any variable is unset
+set -u  # or set -o nounset
 
-set -u # Treat unset variables as an error
+# Ensure required environment variables are set
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 : "$NAME"
 : "$DIRECTORY"
 
-# Debug: Log the values of the variables
+# Print debug information (optional)
 echo "Building Docker image for $CONTAINER_REGISTRY/$NAME:$VERSION from directory $DIRECTORY"
 
-# Run the Docker build command
+# Build the Docker image
 docker build -t "$CONTAINER_REGISTRY/$NAME:$VERSION" --file "./$DIRECTORY/Dockerfile" "./$DIRECTORY"
 
 # Check if the build was successful
-if [ $? -ne 0 ]; then
-  echo "Docker build failed for $CONTAINER_REGISTRY/$NAME:$VERSION"
-  exit 1
+if [ $? -eq 0 ]; then
+    echo "Docker build successful for $CONTAINER_REGISTRY/$NAME:$VERSION"
+else
+    echo "Docker build failed for $CONTAINER_REGISTRY/$NAME:$VERSION"
+    exit 1
 fi
