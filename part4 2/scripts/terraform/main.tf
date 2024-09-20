565855061268d2cf9e6c836c7489de5bf7c7b167
vivekolladapu <68852720+vivekolladapu@users.noreply.github.com>
2024-09-20 18:53:42 +1000

    Update main.tf

--- part4 2/scripts/terraform/main.tf
+++ part4 2/scripts/terraform/main.tf
@@ -21,7 +21,7 @@ variable "kubernetes_version" {
 }
 
 # Creates a resource group for our two services in Azure account.
-resource "azurerm_resource_group" "deakinuni" {
+resource "azurerm_resource_group" "vivek" {
   name     = var.app_name
   location = var.location
 }
@@ -30,7 +30,7 @@ resource "azurerm_resource_group" "deakinuni" {
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                = var.app_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.bmok1.name
+  resource_group_name = azurerm_resource_group.vivek.name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
 
@@ -48,7 +48,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 # Creates a container registry on Azure so that we can publish Docker images.
 resource "azurerm_container_registry" "container_registry" {
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.bmok1.name
+  resource_group_name = azurerm_resource_group.vivek.name
   location            = var.location
   admin_enabled       = true
   sku                 = "Basic"
